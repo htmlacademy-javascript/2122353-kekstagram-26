e779bcd88e252cd598366c5128964f295840af51
Irina <ivbalyuk@gmail.com>
2022-07-08 01:44:06 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 import { getRandomItems } from './data.js';
+import { } from './picture.js';
 
-getRandomItems();
+const items = getRandomItems();
+const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const itemsFragment = document.createDocumentFragment();
+items.forEach((item) => {
+  const itemElement = imageTemplate.cloneNode(true);
+  itemElement.querySelector('.picture__img').src = item.url;
+  itemElement.querySelector('.picture__info')
+    .querySelector('.picture__likes').textContent = item.likes;
+
+  itemElement.querySelector('.picture__info')
+    .querySelector('.picture__comments').textContent = item.comments.length;
+
+  itemsFragment.appendChild(itemElement);
+});
+
+document.querySelector('.pictures').appendChild(itemsFragment);
