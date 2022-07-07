d85cbc399883c751ad87480e40c6a8b2b7d2c6eb
Irina <ivbalyuk@gmail.com>
2022-07-08 02:02:29 +0300

    Отрисуй меня полностью (часть 1) исправления

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 import { getRandomItems } from './data.js';
-import { } from './picture.js';
+import {drawItemPicture } from './picture.js';
 
 const items = getRandomItems();
-const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const itemsFragment = document.createDocumentFragment();
 items.forEach((item) => {
-  const itemElement = imageTemplate.cloneNode(true);
-  itemElement.querySelector('.picture__img').src = item.url;
-  itemElement.querySelector('.picture__info')
-    .querySelector('.picture__likes').textContent = item.likes;
-
-  itemElement.querySelector('.picture__info')
-    .querySelector('.picture__comments').textContent = item.comments.length;
-
-  itemsFragment.appendChild(itemElement);
+  const itemPicture = drawItemPicture(item);
+  itemsFragment.appendChild(itemPicture);
 });
 
 document.querySelector('.pictures').appendChild(itemsFragment);
